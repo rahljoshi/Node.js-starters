177e350450c0406c883d8a57fc50fed8a58a92ef
Rahul Joshi <mrjoshi0147@gmail.com>
2021-07-22 05:58:05 +0530

    delete playground

--- playground/1-json.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-const databuffer = fs.readFileSync("1-json.json");
-const data = JSON.parse(databuffer.toString());
-data.name = "Rahul";
-data.age = 19;
-const json = JSON.stringify(data);
-fs.writeFileSync("1-json.json", json);
